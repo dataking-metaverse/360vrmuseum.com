02c18b43c2a525f99bcfffdb9306a6a2d3c1f741
Wing Him Liu <winghim@dataking.co.kr>
2019-02-28 15:15:02 +0900

    adding comments

--- resources/js/styling/gridTheme.js
+++ resources/js/styling/gridTheme.js
@@ -1,22 +1,18 @@
+// reference: https://www.npmjs.com/package/styled-bootstrap-grid
+
+
 const gridTheme = {
     breakpoints: {
 
     },
     row: {
-        // padding: 10,
+
     },
     col: {
-        // padding: 5,
+
     },
     container: {
         padding: 30,
-        // maxWidth: {
-        //     xl: 1140,
-        //     lg: 960,
-        //     md: 720,
-        //     sm: 540,
-        //     xs: 540,
-        // },
     },
 };
 export default gridTheme;
