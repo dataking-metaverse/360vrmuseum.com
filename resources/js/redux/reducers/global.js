ad7cac9511783e1648fa2da6f5886b5e5726d4a2
Wing Him Liu <winghim@dataking.co.kr>
2019-02-28 10:53:37 +0900

    oh god.....I finally solved all flow errors :(

--- resources/js/redux/reducers/global.js
+++ resources/js/redux/reducers/global.js
@@ -9,27 +9,27 @@ import {
 import type {Action} from "../../types/redux";
 
 
-export const config = (state: {}, action: Action) => {
+export const config = (state: {} | void, action: Action) => {
     if (action.type === CONFIG_REGISTER) { return action.value; }
     return state || {};
 };
 
-export const app = (state: {}, action: Action) => {
+export const app = (state: {} | void, action: Action) => {
     if (action.type === APP_REGISTER) { return action.value; }
     return state || {};
 };
 
-export const assets = (state: {}, action: Action) => {
+export const assets = (state: {} | void, action: Action) => {
     if (action.type === ASSETS_REGISTER) { return action.value; }
     return state || {};
 };
 
-export const lang = (state: {}, action: Action) => {
+export const lang = (state: {} | void, action: Action) => {
     if (action.type === LANG_REGISTER) { return action.value; }
     return state || {};
 };
 
-export const locale = (state: {}, action: Action) => {
+export const locale = (state: {} | void, action: Action) => {
     if (action.type === LOCALE_REGISTER) { return action.value; }
     return state || null;
 };
--- resources/js/redux/reducers/index.js
+++ resources/js/redux/reducers/index.js
@@ -3,7 +3,11 @@ import * as global from "./global";
 import * as home from "./home";
 import type {CombinedReducer} from "redux";
 
-const reducers: CombinedReducer<{}, {}> = combineReducers({
+const reducers: CombinedReducer<{
+    app: {},
+    config: {},
+    assets: {},
+}, {}> = combineReducers({
     ...global,
     ...home,
 });
