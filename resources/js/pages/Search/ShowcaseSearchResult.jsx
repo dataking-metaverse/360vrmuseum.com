11e82a7cc74cb2918e2951ef1160aad946e18dc2
이상명 <smlee@dataking.co.kr>
2019-03-18 16:23:21 +0900

    Left about 1:30...

--- resources/js/pages/Search/ShowcaseSearchResult.jsx
+++ resources/js/pages/Search/ShowcaseSearchResult.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import {Row, Col} from "styled-bootstrap-grid";
 import {connect} from "react-redux";
 import * as R from "ramda";
-import {Link} from "react-router-dom";
+import {Link as RouterLink} from "react-router-dom";
 
 import Showcase from "../../models/Showcase";
 
@@ -19,19 +19,41 @@ type Props = {
 
 
 const Root = styled.div`
-
+    margin-top: 6.5rem;
 `;
 
 const Title = styled.div`
-    
+    color: rgb(51, 51, 51);
+    font-size: 1.8rem;
+    line-height: 2.7rem;
+    font-weight: 700;
+    margin-bottom: 2.5rem;
 `;
 
 const Subtitle = styled.div`
-
+    margin-bottom: 2rem;
+    color: #747474;
+    font-size: 1.3rem;
+    font-family: "Noto Sans";
+    font-weight: 400;  
 `;
 
 const Details = styled.div`
-    
+    font-size: 1.2rem;
+    border-top: 1px solid rgba(0,0,0,.11);
+    border-bottom: 1px solid rgba(0,0,0,.11);
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    color: rgb(116, 116, 116);
+`;
+
+const Link = styled(RouterLink)`
+    color: #333;
+    text-decoration: none;
+    transition: color .4s;
+    &:hover {
+        color: #777777;
+    }
 `;
 
 
