5db8af197824690d83fc705532b365f0c4263900
Wing Him Liu <winghim@dataking.co.kr>
2019-03-19 10:22:11 +0900

    added some cases for exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use App\Exceptions\Api\Exception as ApiException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +47,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+
+        if ($exception instanceof ApiException) {
+            return response()->json($exception);
+        }
+
         return parent::render($request, $exception);
     }
 }
