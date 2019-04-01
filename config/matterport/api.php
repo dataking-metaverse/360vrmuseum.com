df8d98e0d2f6ba8d775ac446af7573fe6fc35012
Wing Him Liu <winghim@dataking.co.kr>
2019-04-01 11:55:57 +0900

    feature : fetch the statistics data from the server and save them into cache for ever

---
+++ config/matterport/api.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'login' => 'https://my.matterport.com/api/v2/user/login/?type=cookie',
+    'models' => [
+        'statistics' => 'https://my.matterport.com/api/v2/models/%s/statistics/',
+    ],
+];
