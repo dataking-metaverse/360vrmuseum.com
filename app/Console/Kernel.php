78c5874219b0808abe41f6393c309415c02d303f
Wing Him Liu <winghim@dataking.co.kr>
2019-04-01 14:25:53 +0900

    hotfix : command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\FetchShowcaseStatistics;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'fetch:showcase:statistics',
+        FetchShowcaseStatistics::class,
     ];
 
     /**
