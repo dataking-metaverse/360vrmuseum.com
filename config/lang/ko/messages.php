3b5e62fa8008336993ba1839fb3795e3ca322e11
Wing Him Liu <winghim@dataking.co.kr>
2019-04-30 01:52:50 +0000

    Update the sentence: to be Korean

--- config/lang/ko/messages.php
+++ config/lang/ko/messages.php
@@ -2,8 +2,8 @@
 
 return [
     'comment' => [
-        'submitted' => 'Comment submitted',
-        'updated' => 'Comment updated',
-        'deleted' => 'Comment deleted',
+        'submitted' => '등록되었습니다',
+        'updated' => '작성하신 댓글이 변경되었습니다',
+        'deleted' => '작성하신 댓글이 삭제되었습니다',
     ],
 ];
