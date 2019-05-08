bc25d21922a7cb07aa52b6eb1d675490b6ca04dd
Wing Him Liu <winghim@dataking.co.kr>
2019-05-08 17:04:18 +0900

    feature : make the national-museum page filterable by url parameter

--- resources/js/pages/NationalMuseum/MuseumsGrid/MuseumGridItems.jsx
+++ resources/js/pages/NationalMuseum/MuseumsGrid/MuseumGridItems.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useContext} from "react";
 import * as R from "ramda";
 import {connect} from "react-redux";
+import {withRouter} from "react-router";
 import {Row, Col} from "styled-bootstrap-grid";
 
 import {registerShowcaseGroupsElements} from "../../../redux/actionBuilders/nationalMuseum";
@@ -17,8 +18,15 @@ type Props = {
     exhibitionGroups: Array<string>,
     showcaseGroupsElements: Array<Node>,
     registerShowcaseGroupsElements: (value: Array<Node>) => void,
+    match: {
+        params: {
+            museumName?: string,
+        },
+    },
 };
 
+
+const getMuseumName: (props: Props) => ?string = R.path(['match', 'params', 'museumName']);
 
 const getGridSize: (showcases: Showcases) => number = R.cond([
     [R.pathEq(['props', 'length'], 1), R.always(3)],
@@ -61,13 +69,15 @@ function renderMuseumGridItem(museumName, showcases: Showcases) {
 function MuseumGridItems(props: Props) {
     const {exhibitionGroups, showcaseGroupsElements, registerShowcaseGroupsElements} = props;
     const {Showcases} = useContext(ModelsContext);
+    const museumName = getMuseumName(props);
+    const pickedExhibitions = typeof museumName === 'undefined' ? exhibitionGroups : [museumName];
 
     useEffect(() => {
         if (!(Array.isArray(showcaseGroupsElements) && showcaseGroupsElements.length > 0)) {
-            Showcases.byPresentedBys(exhibitionGroups)
+            Showcases.byPresentedBys(pickedExhibitions)
                 .then(R.pipe(
                     R.toPairs,
-                    R.sort(([a], [b]) => exhibitionGroups.indexOf(a) - exhibitionGroups.indexOf(b)), // ramda should be able to solve this, rather than self composed
+                    R.sort(([a], [b]) => pickedExhibitions.indexOf(a) - pickedExhibitions.indexOf(b)), // ramda should be able to solve this, rather than self composed
                     R.map(R.apply(renderMuseumGridItem)),
                     registerShowcaseGroupsElements
                 ));
@@ -84,5 +94,6 @@ export default R.compose(
             showcaseGroupsElements: R.prop('showcaseGroupsElements'),
         }),
         R.applySpec({registerShowcaseGroupsElements})
-    )
+    ),
+    withRouter
 )(MuseumGridItems);
