12c251955d8d6688de74c3df24674057bc87b5d3
Wing Him Liu <winghim@dataking.co.kr>
2019-05-14 10:50:23 +0900

    refactoring : separate some code out from there

--- resources/js/TelescopeApp/Main/ShowcasesGrid/styled.jsx
+++ resources/js/TelescopeApp/Main/ShowcasesGrid/styled.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import * as R from "ramda";
 
+const half = R.divide(R.__, 2);
 
 const gutterWidth = R.path(['theme', 'showcasesGrid', 'gutterWidth']);
-const gutterWidthHalf = R.pipe(gutterWidth, R.divide(R.__, 2));
+const gutterWidthHalf = R.pipe(gutterWidth, half);
 
 export const Root = styled.div`
     margin-left: -${gutterWidthHalf}rem;
