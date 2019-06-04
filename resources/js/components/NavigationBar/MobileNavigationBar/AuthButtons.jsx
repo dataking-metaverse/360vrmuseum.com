6adbc290a3858db3fa5a587f899f99b54725fb83
Wing Him Liu <winghim@dataking.co.kr>
2019-06-04 16:23:34 +0900

    refactoring : remove all the react redux hoc-decorator, using hooks instead

--- resources/js/components/NavigationBar/MobileNavigationBar/AuthButtons.jsx
+++ resources/js/components/NavigationBar/MobileNavigationBar/AuthButtons.jsx
@@ -1,37 +1,38 @@
 import * as R from "ramda";
-import {connect} from "react-redux";
 import React from "react";
 
 import {
     Item,
 } from "./styled";
 import LogoutButton from "./LogoutButton";
+import useReduxState from "../../../hooks/useReduxState";
 
-type AuthButtonsProps = {
-    user: User,
-    show: boolean,
-    loginRoute: {|
-        name: string,
-        title: string,
-        to: string,
-    |}
-};
+type Props = {|  |};
 
-const AuthButtons = R.compose(
-    connect(R.applySpec({
-        user: R.prop('user'),
-        show: R.path(['config', 'navigationBar', 'showAuth']),
-        loginRoute: R.applySpec({
-            name: R.path(['config', 'navigationBar', 'login']),
-            title: R.path(['lang', 'navigation', 'login', 'title']),
-            to: R.path(['app', 'routes', 'login']),
-        }),
-    }))
-)(function AuthButtons(props: AuthButtonsProps) {
-    const {user, show, loginRoute} = props;
+type LoginButtonProps = {|  |};
+
+const useLoginRoute = R.pipe(
+    useReduxState,
+    R.applySpec({
+        title: R.path(['lang', 'navigation', 'login', 'title']),
+        to: R.path(['app', 'routes', 'login']),
+    })
+);
+
+const useShowNav = R.pipe(
+    useReduxState,
+    R.path(['config', 'navigationBar', 'showAuth']),
+);
+
+function LoginButton(props: LoginButtonProps) {
+    const {to, title} = useLoginRoute();
+    return <Item to={to}>{title}</Item>;
+}
+
+export default function AuthButtons(props: Props) {
+    const {user} = useReduxState();
+    const show = useShowNav();
     if (!show) { return null; }
-    if (!user) { return <Item to={loginRoute.to}>{loginRoute.title}</Item>; }
+    if (!user) { return <LoginButton />; }
     return <LogoutButton />;
-});
-
-export default AuthButtons;
+}
--- resources/js/components/NavigationBar/MobileNavigationBar/LogoutButton.jsx
+++ resources/js/components/NavigationBar/MobileNavigationBar/LogoutButton.jsx
@@ -1,34 +1,24 @@
 import React from "react";
-import {connect} from "react-redux";
 import * as R from "ramda";
 import {Axios} from "axios";
 
 import {clearUser} from "~/redux/actionCreators/global";
 import {Submit} from "./styled";
+import useReduxState from "../../../hooks/useReduxState";
+import useRoute from "../../../hooks/useRoute";
 
 
-type LogoutFormProps = {|
-    axios: Axios,
-    submitRoute: string,
-    logoutRoute: string,
-    clearUser: () => void,
-|};
+type Props = {|  |};
 
-const LogoutButton = R.compose(
-    connect(
-        R.applySpec({
-            axios: R.prop('axios'),
-            submitRoute: R.path(['app', 'routes', 'api.auth.logout']),
-            logoutRoute: R.applySpec({
-                name: R.path(['config', 'navigationBar', 'logout']),
-                title: R.path(['lang', 'navigation', 'logout', 'title']),
-                to: R.path(['app', 'routes', 'logout']),
-            }),
-        }),
-        R.applySpec({clearUser}),
-    )
-)(function(props: LogoutFormProps) {
-    const {axios, submitRoute, logoutRoute, clearUser} = props;
+const useLogoutTitle = R.pipe(
+    useReduxState,
+    R.path(['lang', 'navigation', 'logout', 'title']),
+);
+
+export default function LogoutButton(props: Props) {
+    const {axios} = useReduxState();
+    const submitRoute = useRoute('api.auth.logout');
+    const logoutTitle = useLogoutTitle();
 
     async function onSubmit(event) {
         event.preventDefault();
@@ -40,12 +30,10 @@ const LogoutButton = R.compose(
     }
 
     return (
-        <React.Fragment>
+        <>
             <form onSubmit={onSubmit}>
-                <Submit type="submit">{logoutRoute.title}</Submit>
+                <Submit type="submit">{logoutTitle}</Submit>
             </form>
-        </React.Fragment>
+        </>
     );
-});
-
-export default LogoutButton;
+}
