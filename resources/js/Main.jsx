2f510235397584b6b01f51676e485fd840c6a68d
winghim-dataking <winghim@dataking.co.kr>
2019-06-17 19:58:28 +0900

    removed the debuggers

--- resources/js/Main.jsx
+++ resources/js/Main.jsx
@@ -5,7 +5,7 @@ import {BaseCSS} from "styled-bootstrap-grid";
 
 import BasicOverridingStyle from "./styling/BasicOverridingStyle";
 import ThirdPartyOverridingStyle from "./styling/ThirdPartyOverridingStyle";
-import DebugBreakpoints from "./styling/DebugBreakpoints";
+// import DebugBreakpoints from "./styling/DebugBreakpoints";
 import theme from "./styling/theme";
 import gridTheme from "./styling/gridTheme";
 import BackToTopButton from "./components/BackToTopButton";
@@ -22,7 +22,7 @@ function StyleSheets() {
             <BaseCSS />
             <BasicOverridingStyle />
             <ThirdPartyOverridingStyle />
-            <DebugBreakpoints />
+            // <DebugBreakpoints />
         </React.Fragment>
     );
 }
