f24f437e0b60b67c39cdba376c52fad54cad3fc9
winghim-dataking <winghim@dataking.co.kr>
2019-06-20 16:48:03 +0900

    [search] Update the search input on the nav bar to be collapsable (desktop only)

--- resources/js/components/NavigationBar/DesktopNavigationBar/SearchWidget.jsx
+++ resources/js/components/NavigationBar/DesktopNavigationBar/SearchWidget.jsx
@@ -1,42 +1,80 @@
 import React from "react";
 import styled from "styled-components";
+import * as R from "ramda";
 
 import SearchSupplier from "~/sideEffectComponents/SearchSupplier";
 import useLangPath from "~/hooks/useLangPath";
+import useBooleanState from "../../../hooks/useBooleanState";
+
+import type {Interpolation} from "styled-components";
 
 type Props = {|  |};
 
+type StyledPropActive = {
+    active?: boolean,
+};
+
+const ifActiveElse = R.ifElse<[StyledPropActive], Interpolation, Interpolation>(R.prop<'active', StyledPropActive>('active'));
+
+const rootColor: Interpolation = ifActiveElse(
+    R.always('#ffffff'),
+    R.always('transparent')
+);
 const Root = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     border-radius: .3rem;
-    background-color: #ffffff;
     padding-left: .6rem;
     padding-right: .6rem;
+    background-color: ${rootColor};
+    transition: background-color .4s;
 `;
 
 const Icon = styled(SearchSupplier.Icon)`
     margin-left: .7rem;
     margin-right: .7rem;
+    cursor: pointer;
 `;
 
+const inputWidth: Interpolation = ifActiveElse(
+    R.always(16.3),
+    R.always(0)
+);
+const inputPaddingX: Interpolation = ifActiveElse(
+    R.always(2.4),
+    R.always(0)
+);
 const Input = styled(SearchSupplier.Input)`
-    width: 16.3rem;
+    width: ${inputWidth}rem;
     line-height: 1;
-    padding: 1.4rem 2.4rem;
     border: none;
     border-radius: .5rem;
     background-color: transparent;
+    padding-top: 1.4rem;
+    padding-bottom: 1.4rem;
+    padding-left: ${inputPaddingX}rem;
+    padding-right: ${inputPaddingX}rem;
+    transition-property: width, padding-left, padding-right;
+    transition-duration: .4s;
 `;
 
+
+const iconColor = R.ifElse(
+    Boolean,
+    R.always(undefined), // to use the components default value
+    R.always('#ffffff')
+);
+
 export default function SearchWidget(props: Props) {
+    const [active,,, toggleActive] = useBooleanState(false);
     const searchText = useLangPath(['common', 'search']);
+    console.log('active', active);
     return (
-            <Root>
+            <Root active={active}>
                 <SearchSupplier>
-                    <Icon />
-                    <Input placeholder={searchText} />
+                    <Icon color={iconColor(active)} onClick={toggleActive} />
+                    <Input active={active} placeholder={searchText} />
                 </SearchSupplier>
             </Root>
     );
