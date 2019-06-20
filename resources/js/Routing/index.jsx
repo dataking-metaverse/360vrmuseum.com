cb93f77603abe4a860c4e8325afbfbd53c994856
winghim-dataking <winghim@dataking.co.kr>
2019-06-20 18:48:30 +0900

    [my account] fix the error that the page would reload pointlessly

--- resources/js/Routing/index.jsx
+++ resources/js/Routing/index.jsx
@@ -14,9 +14,10 @@ import useLang from "~/hooks/useLang";
 import useReduxState from "~/hooks/useReduxState";
 import routes from "./routes";
 
-import type {ComponentType, Node, Element} from "react";
+import type {ComponentType, Node} from "react";
 import type {Props as PageProps} from "~/components/Page";
 
+type RoutePaths = {[string]: string};
 
 type RouteParams = {|
     key: string,
@@ -25,16 +26,20 @@ type RouteParams = {|
     component: ComponentType<PageProps>,
 |};
 
-type Props = {
+type Props = {|  |};
 
-};
+type ViewProps = {|
+    ssr: boolean,
+    routePaths: RoutePaths,
+    lang: {},
+|};
 
 function refineRoute(route: string): string {
     if (typeof route !== 'string') { return '/'; }
     return route[0] !== '/' ? '/' + route : route;
 }
 
-function makeRouteParams(routeUris: {[string]: string}, routeParams): Array<RouteParams> {
+function makeRouteParams(routeUris: RoutePaths, routeParams): Array<RouteParams> {
     const output = [];
     for(let key in routeParams) {
         if (!routeUris.hasOwnProperty(key) || !routeParams.hasOwnProperty(key)) { continue; }
@@ -49,47 +54,65 @@ function makeRouteParams(routeUris: {[string]: string}, routeParams): Array<Rout
     return output;
 }
 
-const renderRoutes = R.map<RouteParams, Node, (routeParams: RouteParams) => Node>(routeParam => React.createElement(Route, routeParam));
+const renderRoutes = R.pipe<[RoutePaths], RouteParams>(
+    makeRouteParams,
+    R.map<RouteParams, Node, (routeParams: RouteParams) => Node>(routeParam => React.createElement(Route, routeParam))
+);
 
 const useRoutePaths = R.pipe(
     useReduxState,
-    R.path<string, {[string]: string}>(['app', 'routes'])
+    R.path<string, RoutePaths>(['app', 'routes'])
 );
+
+/**
+ * NOTE : build a pure component here to stop rerender with the same set of props
+ */
+class RoutingPureView extends React.PureComponent<ViewProps> {
+    render() {
+        const {ssr, routePaths, lang} = this.props;
+        const routeContent = renderRoutes(routePaths, routes);
+        const content = (
+            <React.Fragment>
+                <RedirectHandler />
+                <LangHandler />
+                <NotificationHandler />
+                <SwitchPageHandler />
+                <RecaptchaHandler />
+                {lang && (
+                    <React.Fragment>
+                        <NavigationBar />
+                        {routeContent}
+                        <Footer />
+                    </React.Fragment>
+                )}
+            </React.Fragment>
+        );
+
+        if (ssr && typeof global.context !== 'undefined') {
+            return (
+                <StaticRouter location={refineRoute(global.context.route)} context={{}}>
+                    {content}
+                </StaticRouter>
+            );
+        }
+        return (
+            <BrowserRouter>
+                {content}
+            </BrowserRouter>
+        );
+    }
+}
 
 export default function Routing(props: Props) {
     const lang = useLang();
     const routePaths = useRoutePaths();
     const {ssr} = useReduxState();
-
     if (!routePaths) { return null; }
-
-    const content = (
-        <React.Fragment>
-            <RedirectHandler />
-            <LangHandler />
-            <NotificationHandler />
-            <SwitchPageHandler />
-            <RecaptchaHandler />
-            {lang && (
-                <React.Fragment>
-                    <NavigationBar />
-                    {renderRoutes(makeRouteParams(routePaths, routes))}
-                    <Footer />
-                </React.Fragment>
-            )}
-        </React.Fragment>
-    );
-
-    if (ssr && typeof global.context !== 'undefined') {
-        return (
-            <StaticRouter location={refineRoute(global.context.route)} context={{}}>
-                {content}
-            </StaticRouter>
-        );
-    }
     return (
-        <BrowserRouter>
-            {content}
-        </BrowserRouter>
+        <RoutingPureView
+            ssr={ssr}
+            routePaths={routePaths}
+            lang={lang}
+        />
     );
-}
+};
