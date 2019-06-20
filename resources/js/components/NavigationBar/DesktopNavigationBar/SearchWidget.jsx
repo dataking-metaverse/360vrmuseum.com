656ec73381387e5313d54f521e0b93998c5c4701
winghim-dataking <winghim@dataking.co.kr>
2019-06-20 16:09:30 +0900

    formatting

--- resources/js/components/NavigationBar/DesktopNavigationBar/SearchWidget.jsx
+++ resources/js/components/NavigationBar/DesktopNavigationBar/SearchWidget.jsx
@@ -22,12 +22,12 @@ const Icon = styled(SearchSupplier.Icon)`
 `;
 
 const Input = styled(SearchSupplier.Input)`
-width: 16.3rem;
-line-height: 1;
-padding: 1.4rem 2.4rem;
-border: none;
-border-radius: .5rem;
-background-color: transparent;
+    width: 16.3rem;
+    line-height: 1;
+    padding: 1.4rem 2.4rem;
+    border: none;
+    border-radius: .5rem;
+    background-color: transparent;
 `;
 
 export default function SearchWidget(props: Props) {
