4aea6a2c522fe8ce6fe6bac813a0e30296f94015
winghim-dataking <winghim@dataking.co.kr>
2019-07-12 17:38:56 +0900

    [telescope] Touching the home logo should be to the home page (index page of exhitibions)

--- resources/js/TelescopeApp/Main/ShowcaseContent/index.jsx
+++ resources/js/TelescopeApp/Main/ShowcaseContent/index.jsx
@@ -17,6 +17,10 @@ import RelatedSection from "./RelatedSection";
 
 import type {Node} from "react";
 import type {Showcase, ReactRef} from "../../types";
+import {Header, Logo} from "../ShowcaseMenu/styled";
+import {scopeLogo} from "../../assets";
+import useReduxAction from "../../hooks/useReduxAction";
+import {emptyShowcase as emptyShowcaseAction} from "../../redux/actionCreators";
 
 type Props = {|  |};
 
@@ -34,6 +38,7 @@ const jumpToTop = R.pipe<[ScrollableRef], null, ?Scrollable, any>(
 export default function ShowcaseContent(props: Props): Node | null {
     const showcase: Showcase = useShowcase();
     const scrollableRef: ScrollableRef = useRef(null);
+    const emptyShowcase = useReduxAction(emptyShowcaseAction);
 
     useEffect(() => {
         jumpToTop(scrollableRef);
@@ -45,6 +50,12 @@ export default function ShowcaseContent(props: Props): Node | null {
             <Scrollable ref={scrollableRef}>
                 <Content>
                     <Container>
+                        <Header>
+                            <Logo
+                                src={scopeLogo}
+                                onClick={emptyShowcase}
+                            />
+                        </Header>
                         <HR.Black />
                         <CloseButtonSection />
                     </Container>
