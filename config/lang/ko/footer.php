e340a023aa7e0a07d7437a7bde45b3ce471db5b5
winghim-dataking <winghim@dataking.co.kr>
2020-07-23 18:16:20 +0900

    feature : update the year on footer's copyright statement

--- config/lang/ko/footer.php
+++ config/lang/ko/footer.php
@@ -3,7 +3,7 @@
 return [
     'privacyPolicy' => '개인정보 이용약관',
     'termsOfConditions' => '서비스 이용약관',
-    'copyright' => 'Copyright ©dataking.Inc 2015 - 2019',
+    'copyright' => 'Copyright ©dataking.Inc 2015 - ' . date('Y'),
     'allRigthReserved' => 'All rights reserved.',
     'noPrinting' => '무단전재 및 재배포금지',
 ];
